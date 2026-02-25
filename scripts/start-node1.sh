0e0956e7b6e772717e9ae300ab9dbe0da13e259a
Thor SV01 <sv01@bolla.network>
2026-02-25 17:22:58 +0000

    chore(chain): add current runtime-ready config and startup scripts

--- scripts/start-node1.sh
+++ scripts/start-node1.sh
@@ -4,11 +4,17 @@ exec /usr/local/bin/polygon-edge server \
   --chain /opt/trn-chain/config/genesis.json \
   --data-dir /opt/trn-chain/node1 \
   --seal \
+<<<<<<< HEAD
   --price-limit 1000000000 \
   --jsonrpc 127.0.0.1:8545 \
   --grpc-address 127.0.0.1:9632 \
   --libp2p 127.0.0.1:1478 \
   --prometheus 127.0.0.1:10000 \
+=======
+  --jsonrpc 127.0.0.1:8545 \
+  --grpc-address 127.0.0.1:9632 \
+  --libp2p 127.0.0.1:1478 \
+>>>>>>> faedf26 (chore(chain): add current runtime-ready config and startup scripts)
   --no-discover \
   --access-control-allow-origins node1.turion.network \
   --log-level INFO \
